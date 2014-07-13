3ec0b27ea340a2b983d9db12ca837319b12f7d32
martina-prp <radeva_14@abv.bg>
2014-07-13 23:32:28 +0300

    Solution for Students-CRUD task.

--- week4/6-Students-CRUD/client/app.js
+++ week4/6-Students-CRUD/client/app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     getAll();
   });
 
-  $('.create').on('click', function(event) {
+  $('.create, .update').on('click', function(event) {
     event.preventDefault();
     var courses = $('#create-courses').val();
     var student = {
@@ -16,8 +16,29 @@ $(document).ready(function() {
       courses: courses.split(','),
     };
     createStudent(student);
+    $('#create-name').val('');
+    $('#create-fn').val('');
+    $('#create-courses').val('');
   });
 
+  $(document).on('click', '.select-update', function(event) {
+    event.preventDefault();
+    var row = $(this).closest('tr');
+    $('input#create-name').val(row.find('>td.student-name').text());
+    $('input#create-fn').val(row.find('>td.student-fn').text());
+    $('input#create-courses').val(row.find('>td.student-courses').text());
+  });
+
+  $(document).on('click', '.select-delete', function(event) {
+    event.preventDefault();
+    var row = $(this).closest('tr');
+    var deleteFnNumber = row.find('>td.student-fn').text();
+    if (confirm('Are you sure you want to delete the student?')) {
+      deleteStudent(deleteFnNumber);
+    }
+  });
+
+  // Make ajax call to get all available students in the dtabase.
   function getAll() {
     $.ajax({
       url: 'http://localhost:3030/students',
@@ -25,14 +46,15 @@ $(document).ready(function() {
       contentType: 'application/json',
       datatype: 'json',
     }).done(function(data){
-      console.log(data);
       if (data.length !== 0) {
         generateStudents(data);
       }
     });
   }
 
-  function createStudent(student) {console.log(student);
+  // Make ajax call for creating new record or updating existing student.
+  // Regenerate the entire table with the created/updated data.
+  function createStudent(student) {
     $.ajax({
       url: 'http://localhost:3030/student',
       type: 'POST',
@@ -45,14 +67,37 @@ $(document).ready(function() {
       })
     }).done(function(data){
       $('.operation-message').text(data.status);
+      if (data.status === 'saved') {
+        getAll();
+      }
     });
   }
 
+  // Make ajax call to delete a student bu faculty name.
+  // Regenerate the entire table without the deleted student.
+  function deleteStudent(fnNumber) {
+    $.ajax({
+      url: 'http://localhost:3030/student/' + fnNumber,
+      type: 'DELETE',
+      contentType: 'application/json',
+      datatype: 'json',
+    }).done(function(data) {
+      $('.operation-message').text(data.status);
+      getAll();
+    });
+  }
+
+  // Generates the table html and appends it to the main container.
   function generateStudents(students) {
     var studentsTemp = $('#students-template').html();
     var template = Handlebars.compile(studentsTemp);
     var studentsHtml = template({students: students});
-    $('.students-container').replaceWith(studentsHtml);
+    var html = ['<table class = "table table-bordered table-hover" id="students-table">',
+      studentsHtml,
+      '</table>'
+      ];
+    $('.students-container').empty();
+    $('.students-container').append(html.join(''));
   }
 
 });
